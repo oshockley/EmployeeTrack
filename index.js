336f1b4ea874e32a3d6d035b4e2ce490120ef5e1
O'Neal Shockley <shockleyoneal@gmail.com>
2025-03-06 20:28:46 -0800

    added changes to index.js

--- index.js
+++ index.js
@@ -98,3 +98,150 @@ function addRole() {
 }
 
 // Function to update employees' roles
+function viewDepartments() {
+    pool.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        startApp();
+    });
+}
+
+//Function to examine all roles
+function viewRoles() {
+    const query = `
+    SELECT role.id, role.title, department.name AS department, role.salary
+    FROM role
+    JOIN department ON role.department_id = department.id;
+    `;
+    pool.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        startApp();
+    });
+}
+
+// Function to examine all employees
+function viewEmployees() {
+    const query = `
+    SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary,
+    COALESCE(m.first_name || ' ' || m.last_name, 'None') AS manager
+    FROM employee e
+    JOIN role ON e.role_id = role.id
+    JOIN department ON role.department_id = departmet.id
+    LEFT JOIN employee m ON e.manager_id = m.id;
+    `;
+    pool.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        startApp();
+    });
+}
+
+// Fucntion to add selected department
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "deptName",
+            message: "Enter the department name:",
+        },
+    ])
+    .then((answer) => {
+        pool.query("INSERT INTO department (name) VALUES ($1)", [answer.deptName], (err) => {
+            if(err) throw err;
+            console.log("Department has been added!");
+            startApp();
+        });
+    });
+}
+
+// Function to add role
+function addRole() {
+    pool.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+
+        const departmentChoices = res.rows.map((dept) => ({
+            name: dept.name,
+            value: dept.id,
+        }));
+
+        inquirer
+            .prompt([
+                { type: "input", name: "title", message: "Enter title of role:"},
+                { type: "input", name: "salary", message: "Enter salary:"},
+                {
+                    type: "list",
+                    name: "department_id",
+                    message: "Select the deparmtent:",
+                    choices: departmentChoices,
+                
+            },
+        ])
+        .then((answer) => {
+            pool.query(
+                "INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)",
+                [answer.title, answer.salary, answer.department_id],
+                (err) => {
+                    if (err) throw err;
+                    console.log("Role has been added!");
+                    startApp();
+                }
+            );
+        });
+    });
+}
+
+// Function to add employee
+function addEmployee() {
+    pool.query("SELECT * FROM role", (err, res) => {
+        if (err) throw err;
+
+        const roleChoices = res.rows.map((role) => ({
+            name: role.title,
+            value: role.id,
+        }));
+        
+        inquirer
+            .prompt([
+                { type: "input", name: "first_name", message: "Enter employee's first name:" },
+                { type: "input", name: "last_name", message: "Enter employee's last name:" },
+                { type: "list", name: "role_id", message: "Select role:", choices: roleChoices },
+                { type: "input", name: "manager_id", message: "Enter manager ID (leave blank):" },
+            ])
+            .then((answer) => {
+                pool.query(
+                    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)",
+                    [answer.first_name, answer.last_name, answer.role_id, answer.manager_id || null],
+                    (err) => {
+                        if (err) throw err;
+                        console.log("Employee has been added!");
+                        startApp();
+                    }
+                );
+            });
+    });
+}
+
+// Update Employee Role
+function updateEmployeeRole() {
+    inquirer
+        .prompt([
+            { type: "input", name: "employee_id", message: "To update, enter employee ID:" },
+            { type: "input", name: "new_role_id", message: "Enter new role ID:" },
+        ])
+        .then((answer) => {
+            pool.query(
+                "UPDATE employee SET role_id = $1 WHERE id = $2",
+                [answer.new_role_id, answer.employee_id],
+                (err) => {
+                    if (err) throw err;
+                    console.log("Employee Role Update Successful!");
+                    startApp();
+                }
+            );
+        });
+}
+
+// Start EmployeeTrack
+startApp();
